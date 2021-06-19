2bb192e5b8a0205a2a36b5e4f2389f8f746d0fc5
Siva Nagi Reddy Alla <siva.alla@outlook.com>
2021-06-19 12:08:38 +0530

    Add favicon

--- src/main/java/com/sivaone/cloudwebapp/config/SecurityConfig.java
+++ src/main/java/com/sivaone/cloudwebapp/config/SecurityConfig.java
@@ -11,7 +11,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/index", "/bootstrap/**").permitAll()
+                .antMatchers("/", "/index", "/bootstrap/**","/favicon.*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
