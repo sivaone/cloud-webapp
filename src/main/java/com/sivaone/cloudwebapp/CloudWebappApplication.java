34d759775b2743a3e8a58f3af8324900c0d5db0a
Siva Nagi Reddy Alla <siva.alla@outlook.com>
2021-06-19 11:00:49 +0530

    Initial commit

---
+++ src/main/java/com/sivaone/cloudwebapp/CloudWebappApplication.java
@@ -0,0 +1,13 @@
+package com.sivaone.cloudwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sivaone/cloudwebapp/CloudWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivaone.cloudwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
